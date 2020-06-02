2f09dc8bf95cceefea8e4bdf709ff64d9cf737a4
Scala Steward <me@scala-steward.org>
2020-06-02 12:16:00 +0200

    Update cats-effect to 2.1.3

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Dependencies {
 
   object V {
     val cats       = "2.1.0"
-    val catsEffect = "2.1.2"
+    val catsEffect = "2.1.3"
     val fs2        = "2.3.0"
     val newtype    = "0.4.3"
     val pulsar     = "2.5.0"
