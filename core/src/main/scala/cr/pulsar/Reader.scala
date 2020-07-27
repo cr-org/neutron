0fab6080848f0ca1f5d64900844e9b4e7450218d
Pavels Sisojevs <p.sisoyev@gmail.com>
2020-07-27 22:59:09 +0200

    Implement Reader

---
+++ core/src/main/scala/cr/pulsar/Reader.scala
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2020 Chatroulette
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cr.pulsar
+
+import cats._
+import cats.effect._
+import cats.implicits._
+import cr.pulsar.internal.FutureLift._
+import fs2._
+import org.apache.pulsar.client.api.{ Message, MessageId }
+import scala.util.control.NoStackTrace
+
+/**
+ * A Reader can be used to read all the messages currently available in a topic.
+ */
+trait Reader[F[_]] {
+  def read: Stream[F, Message[Array[Byte]]]
+}
+
+object Reader {
+
+  case class DecodingFailure(msg: String) extends NoStackTrace
+
+  private def mkReader[F[_]: Concurrent: ContextShift](
+      client: PulsarClient.T,
+      topic: Topic
+  ): Resource[F, Reader[F]] =
+    Resource
+      .make {
+        F.delay {
+          client.newReader
+            .topic(topic.url.value)
+            .startMessageId(MessageId.earliest)
+            .create()
+        }
+      }(
+        c => F.delay(c.closeAsync()).void
+      )
+      .map { c =>
+        new Reader[F] {
+          override def read: Stream[F, Message[Array[Byte]]] =
+            Stream.repeatEval(
+              F.delay(c.readNextAsync()).futureLift
+            )
+        }
+      }
+
+  /**
+    * It creates a simple [[Reader]].
+    */
+  def create[F[_]: Concurrent: ContextShift](
+      client: PulsarClient.T,
+      topic: Topic
+  ): Resource[F, Reader[F]] =
+    mkReader[F](client, topic)
+
+  /**
+    * A simple message decoder that uses a [[cats.Inject]] instance
+    * to deserialise consumed messages.
+    *
+    * Consumed messages will be logged using the given `logAction`.
+    */
+  def loggingMessageDecoder[
+      F[_]: MonadError[*[_], Throwable]: Parallel,
+      E: Inject[*, Array[Byte]]
+  ](
+      logAction: E => Topic.URL => F[Unit]
+  ): Pipe[F, Message[Array[Byte]], E] =
+    _.evalMap { m =>
+      val data = m.getData
+
+      E.prj(data) match {
+        case Some(e) =>
+          logAction(e)(Topic.URL(m.getTopicName)).as(e)
+        case None =>
+          DecodingFailure(new String(data, "UTF-8")).raiseError[F, E]
+      }
+    }
+
+  /**
+    * A simple message decoder that uses a [[cats.Inject]] instance
+    * to deserialise consumed messages.
+    *
+    * Messages will not be logged by default.
+    */
+  def messageDecoder[
+      F[_]: MonadError[*[_], Throwable]: Parallel,
+      E: Inject[*, Array[Byte]]
+  ]: Pipe[F, Message[Array[Byte]], E] =
+    loggingMessageDecoder[F, E](_ => _ => F.unit)
+
+}
