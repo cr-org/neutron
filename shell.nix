4cf8d0b809914f9d2abf858443b757c2edfe8a71
Gabriel Volpe <volpegabriel@gmail.com>
2020-09-18 08:35:24 +0200

    Update default to JDK11

--- shell.nix
+++ shell.nix
@@ -4,12 +4,13 @@ let
     url    = "https://github.com/NixOS/nixpkgs-channels/archive/6bcb1dec8ea.tar.gz";
     sha256 = "04x750byjr397d3mfwkl09b2cz7z71fcykhvn8ypxrck8w7kdi1h";
   };
-  pkgs = import nixpkgs {};
+  config = {
+    packageOverrides = p: {
+      sbt = p.sbt.override { jre = p.jdk11; };
+    };
+  };
+  pkgs = import nixpkgs { inherit config; };
 in
   pkgs.mkShell {
-    buildInputs = with pkgs; [
-      git     # v2.26.2
-      openjdk # v1.8.0_242
-      sbt     # v1.3.10
-    ];
+    buildInputs = [ pkgs.jdk11 pkgs.sbt ];
   }
