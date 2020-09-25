38c470a8dba9c1064893cbecb7bfa398f974fbb6
Gabriel Volpe <volpegabriel@gmail.com>
2020-09-25 15:00:38 +0200

    Use Cachix for sbt derivations

--- shell.nix
+++ shell.nix
@@ -1,27 +1,7 @@
 { java ? "jdk11" }:
 
 let
-  nixpkgs = fetchTarball {
-    name   = "NixOS-unstable-13-05-2020";
-    url    = "https://github.com/NixOS/nixpkgs-channels/archive/6bcb1dec8ea.tar.gz";
-    sha256 = "04x750byjr397d3mfwkl09b2cz7z71fcykhvn8ypxrck8w7kdi1h";
-  };
-
-  config = {
-    packageOverrides = p: {
-      sbt = p.sbt.overrideAttrs (
-        old: rec {
-          version = "1.3.13";
-          sbtJdk  = p.${java};
-          patchPhase = ''
-            echo -java-home ${sbtJdk} >> conf/sbtopts
-          '';
-        }
-      );
-    };
-  };
-
-  pkgs = import nixpkgs { inherit config; };
+  pkgs = import ./pkgs.nix { inherit java; };
 in
   pkgs.mkShell {
     buildInputs = [
