21e71e9eee74f6298b0ce64bdb08d6cf2d27d88e
Scala Steward <me@scala-steward.org>
2020-10-09 16:35:30 +0200

    Update munit, munit-scalacheck to 0.7.14

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
     val catsEffect = "2.2.0"
     val circe      = "0.13.0"
     val fs2        = "2.4.4"
-    val munit      = "0.7.13"
+    val munit      = "0.7.14"
     val newtype    = "0.4.4"
     val pulsar     = "2.6.1"
 
