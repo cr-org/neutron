c7ce5c28f29cb71a63f310ea3073a38373a41ac5
Gabriel Volpe <volpegabriel@gmail.com>
2021-03-25 10:11:40 +0100

    Fix "the input device is not a TTY" error

--- run.sh
+++ run.sh
@@ -29,15 +29,15 @@ wait_for_pulsar(){
 wait_for_pulsar
 
 echo "Creating the public/neutron namespace"
-docker-compose exec pulsar bin/pulsar-admin namespaces create public/neutron
+docker-compose exec -T pulsar bin/pulsar-admin namespaces create public/neutron
 
 echo "Setting BACKWARD schema compatibility strategy for the public/neutron namespace"
-docker-compose exec pulsar bin/pulsar-admin namespaces set-schema-compatibility-strategy -c BACKWARD public/neutron
+docker-compose exec -T pulsar bin/pulsar-admin namespaces set-schema-compatibility-strategy -c BACKWARD public/neutron
 
 echo "Creating the public/nope namespace"
-docker-compose exec pulsar bin/pulsar-admin namespaces create public/nope
+docker-compose exec -T pulsar bin/pulsar-admin namespaces create public/nope
 
 echo "Setting ALWAYS_INCOMPATIBLE schema compatibility strategy for the public/nope namespace"
-docker-compose exec pulsar bin/pulsar-admin namespaces set-schema-compatibility-strategy -c ALWAYS_INCOMPATIBLE public/nope
+docker-compose exec -T pulsar bin/pulsar-admin namespaces set-schema-compatibility-strategy -c ALWAYS_INCOMPATIBLE public/nope
 
 echo "Done"
