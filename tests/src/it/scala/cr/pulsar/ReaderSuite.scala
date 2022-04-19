1ecc1ca3b49e733908f659cc62a6508801639c7f
Pavels Sisojevs <p.sisoyev@gmail.com>
2022-04-19 22:30:10 +0200

    Remove debug logs

--- tests/src/it/scala/cr/pulsar/ReaderSuite.scala
+++ tests/src/it/scala/cr/pulsar/ReaderSuite.scala
@@ -6,38 +6,30 @@ import cr.pulsar.domain.Event
 import cr.pulsar.schema.circe._
 
 object ReaderSuite extends NeutronSuite {
-  (1 to 200).toList.foreach { i =>
-    test("Reader can check if topic has messages " + i) { client =>
-      val topic = mkTopic
-      println(s"Topic1 ${i}: " + topic.url.value)
-      val event = mkEvent
+  test("Reader can check if topic has messages") { client =>
+    val topic = mkTopic
+    val event = mkEvent
 
-      val resources = for {
-        prod <- Producer.make[IO, Event](client, topic)
-        reader <- Reader.make[IO, Event](client, topic)
-      } yield prod -> reader
+    val resources = for {
+      prod <- Producer.make[IO, Event](client, topic)
+      reader <- Reader.make[IO, Event](client, topic)
+    } yield prod -> reader
 
-      resources.use {
-        case (producer, reader) =>
-          for {
-            res1 <- reader.messageAvailable
-            _ <- if (res1 == MessageAvailable.Yes) reader.read1.flatMap { e =>
-                  IO.println(s"${i} Received unexpected event: ${e}, expected: ${event}")
-                }
-                else IO.println(s"${i} As expected")
-            _ <- producer.send(event)
-            res2 <- reader.messageAvailable
-          } yield {
-            expect.same(MessageAvailable.No, res1) &&
-            expect.same(MessageAvailable.Yes, res2)
-          }
-      }
+    resources.use {
+      case (producer, reader) =>
+        for {
+          res1 <- reader.messageAvailable
+          _ <- producer.send(event)
+          res2 <- reader.messageAvailable
+        } yield {
+          expect.same(MessageAvailable.No, res1) &&
+          expect.same(MessageAvailable.Yes, res2)
+        }
     }
   }
 
   test("Reader can read a message if it exists") { client =>
     val topic = mkTopic
-    println("Topic2: " + topic.url.value)
     val event = mkEvent
 
     val resources = for {
