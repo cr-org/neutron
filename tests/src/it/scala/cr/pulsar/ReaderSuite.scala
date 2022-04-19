5fa306f8c6391aede121de0fce093e7dc3c29d22
Pavels Sisojevs <p.sisoyev@gmail.com>
2022-04-19 21:29:49 +0200

    Add debug logs (#303)
    * Add debug logs
    
    * Remove debug logs

--- tests/src/it/scala/cr/pulsar/ReaderSuite.scala
+++ tests/src/it/scala/cr/pulsar/ReaderSuite.scala
@@ -8,24 +8,24 @@ import cr.pulsar.schema.circe._
 object ReaderSuite extends NeutronSuite {
   test("Reader can check if topic has messages") { client =>
     val topic = mkTopic
+    val event = mkEvent
 
     val resources = for {
       prod <- Producer.make[IO, Event](client, topic)
       reader <- Reader.make[IO, Event](client, topic)
     } yield prod -> reader
 
-    resources
-      .use {
-        case (producer, reader) =>
-          for {
-            res1 <- reader.messageAvailable
-            _ <- producer.send(mkEvent)
-            res2 <- reader.messageAvailable
-          } yield {
-            expect.same(MessageAvailable.No, res1) &&
-            expect.same(MessageAvailable.Yes, res2)
-          }
-      }
+    resources.use {
+      case (producer, reader) =>
+        for {
+          res1 <- reader.messageAvailable
+          _ <- producer.send(event)
+          res2 <- reader.messageAvailable
+        } yield {
+          expect.same(MessageAvailable.No, res1) &&
+          expect.same(MessageAvailable.Yes, res2)
+        }
+    }
   }
 
   test("Reader can read a message if it exists") { client =>
@@ -37,17 +37,16 @@ object ReaderSuite extends NeutronSuite {
       reader <- Reader.make[IO, Event](client, topic)
     } yield prod -> reader
 
-    resources
-      .use {
-        case (producer, reader) =>
-          for {
-            res1 <- reader.read1
-            _ <- producer.send(event)
-            res2 <- reader.read1
-          } yield {
-            expect.same(None, res1) &&
-            expect.same(Some(event), res2)
-          }
-      }
+    resources.use {
+      case (producer, reader) =>
+        for {
+          res1 <- reader.read1
+          _ <- producer.send(event)
+          res2 <- reader.read1
+        } yield {
+          expect.same(None, res1) &&
+          expect.same(Some(event), res2)
+        }
+    }
   }
 }
