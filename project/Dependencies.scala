62c84c716b03f20413de976fc693c5ce86c1ed8b
Pavels Sisojevs <p.sisoyev@gmail.com>
2022-04-19 22:46:46 +0200

    Update fs2-core to 3.2.7 (#291)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -9,7 +9,7 @@ object Dependencies {
     val cats       = "2.7.0"
     val catsEffect = "3.3.11"
     val circe      = "0.14.1"
-    val fs2        = "3.2.2"
+    val fs2        = "3.2.7"
     val newtype    = "0.4.4"
     val pulsar     = "2.7.4"
     val weaver     = "0.7.11"
