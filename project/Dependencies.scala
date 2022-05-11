619758aa90ae695532394cd5169f9259e6c22b23
Pavels Sisojevs <p.sisoyev@gmail.com>
2022-05-11 00:44:08 +0000

    Update avro4s-core to 4.0.13

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,8 +5,8 @@ object Dependencies {
   object V {
     val java8Compat = "1.0.2"
 
-    val avro4s     = "4.0.12"
-    val cats       = "2.7.0"
+    val avro4s = "4.0.13"
+    val cats   = "2.7.0"
 
     val catsEffect = "3.3.11"
     val circe      = "0.14.1"
