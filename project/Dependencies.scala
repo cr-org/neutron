883c22107f5c53ed7bf08fad62b28924fa61787b
Pavels Sisojevs <p.sisoyev@gmail.com>
2022-06-30 00:42:51 +0000

    Update weaver-cats, weaver-scalacheck to 0.7.13

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -13,7 +13,7 @@ object Dependencies {
     val fs2        = "3.2.8"
     val newtype    = "0.4.4"
     val pulsar     = "2.7.4"
-    val weaver     = "0.7.12"
+    val weaver     = "0.7.13"
 
     val betterMonadicFor = "0.3.1"
     val contextApplied   = "0.1.4"
