6090ea2c1eac81e29e8a092a7005e7ea6a8c3cbd
Pavels Sisojevs <p.sisoyev@gmail.com>
2025-04-08 00:07:07 +0000

    Update cats-effect to 3.6.1

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
     val avro4s = "4.1.2"
 
     val cats       = "2.13.0"
-    val catsEffect = "3.5.7"
+    val catsEffect = "3.6.1"
     val fs2        = "3.11.0"
     val circe      = "0.14.10"
 
